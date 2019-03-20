3047ccae0327ec83c570fda7fe832e5666d94571
Sergio Ortiz Crespo <sergiooc@gmail.com>
2019-03-20 17:54:35 +0100

    delete comment clean

--- webpack.config.js
+++ webpack.config.js
@@ -51,7 +51,7 @@ module.exports = {
     ]
   },
   plugins: [
-    new CleanWebpackPlugin(),
+    // new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       filename: "home.html",
       template: "./src/home.htm",
